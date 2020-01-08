7ae8dd5ee3842bbd6be26beada78137f273a2da8
Shirkit <shirkit@gmail.com>
2020-01-08 12:51:40 -0300

    Indentation implementation

--- assets/coupons.js
+++ assets/coupons.js
@@ -1,14 +1,14 @@
 jQuery(document).ready(function($) {
-	if (Array.isArray(pos_coupon_presets)) {
-		$('#modal-order_discount #coupon_tab .wrap-button').before('<div class="wrap-button wrap-custom-button-discount" ></div>');
-		var wrapper = $('#modal-order_discount #coupon_tab .wrap-custom-button-discount');
-		for (var i = 0; i < pos_coupon_presets.length; i++) {
-			$(wrapper).append('<button class="button button-primary wp-button-large alignright" type="button" name="' + pos_coupon_presets[i] + '"> ' + pos_coupon_presets[i] + '</button></div>');
-		}
-		$('#modal-order_discount #coupon_tab .wrap-custom-button-discount button').each(function (a) {
-			$(this).click(function(e) {
-				CART.add_discount($(this).attr('name'));
-			});
-		});
-	}
+  if (Array.isArray(pos_coupon_presets)) {
+    $('#modal-order_discount #coupon_tab .wrap-button').before('<div class="wrap-button wrap-custom-button-discount" ></div>');
+    var wrapper = $('#modal-order_discount #coupon_tab .wrap-custom-button-discount');
+    for (var i = 0; i < pos_coupon_presets.length; i++) {
+      $(wrapper).append('<button class="button button-primary wp-button-large alignright" type="button" name="' + pos_coupon_presets[i] + '"> ' + pos_coupon_presets[i] + '</button></div>');
+    }
+    $('#modal-order_discount #coupon_tab .wrap-custom-button-discount button').each(function(a) {
+      $(this).click(function(e) {
+        CART.add_discount($(this).attr('name'));
+      });
+    });
+  }
 });
